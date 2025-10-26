aaca53df06ec948d5475e807691c84e70c9d3b9d
masato-bkn <37011138+masato-bkn@users.noreply.github.com>
2025-10-26 13:18:12 +0900

    不要なサービスを削除

--- src/main/kotlin/service/GreetingService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.service
-
-class GreetingService {
-    companion object {
-        fun generateGreeting(): String {
-            return "Hello World from Kotlin Lambda!"
-        }
-    }
-}
